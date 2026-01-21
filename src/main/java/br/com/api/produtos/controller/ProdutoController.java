e8d549c2ce914ec7d301f0d856ecf70970014455
Dev-Isaque <isaquesbl123@gmail.com>
2026-01-21 16:45:56 -0300

    Adding controller, service, and model to the product software

--- src/main/java/br/com/api/produtos/controller/ProdutoController.java
+++ src/main/java/br/com/api/produtos/controller/ProdutoController.java
@@ -1,17 +1,50 @@
 package br.com.api.produtos.controller;
 
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController /*  */
-@RequestMapping("/api/produtos")
+import br.com.api.produtos.model.ProdutoModel;
+import br.com.api.produtos.service.ProdutoService;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class ProdutoController {
 
-    @GetMapping
-    public List<String> listar() {
-        return List.of("Mouse", "Teclado", "Monitor");
+    @Autowired
+    private ProdutoService ps;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModel pm) {
+        return ps.cadastrar(pm);
+    }
+
+    @PostMapping("/atualizar/{id}")
+    public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody ProdutoModel pm) {
+        pm.setId(id);
+        return ps.atualizar(pm);
+    }
+
+    @DeleteMapping("/remover/{id}")
+    public ResponseEntity<?> remover(@PathVariable Long id) {
+        return ps.remover(id);
+    }
+
+    @GetMapping("/listarTodos")
+    public Iterable<ProdutoModel> listarTodos() {
+        return ps.listarTodos();
+    }
+
+    @GetMapping("/")
+    public String rota() {
+        return "API de Produtos funcionando!";
     }
 }
--- src/main/java/br/com/api/produtos/model/ProdutoModel.java
+++ src/main/java/br/com/api/produtos/model/ProdutoModel.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "produtos")
-public class ProdutoModelo {
+public class ProdutoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
