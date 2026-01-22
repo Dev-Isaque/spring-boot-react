ce3f9caaa828ffd74870701f44f5ca6b83d9da8b
Dev-Isaque <isaquesbl123@gmail.com>
2026-01-22 16:59:10 -0300

    Study project successfully completed.

--- front-end/src/Formulario.js
+++ front-end/src/Formulario.js
@@ -1,6 +1,20 @@
-function Formulario() {
+function Formulario({ botao, eventoTeclado, cadastrar, obj, cancelar, remover, alterar }) {
     return (
-        <h1>Formulario</h1>
+        <form>
+            <input type='text' onChange={eventoTeclado} name='nome' placeholder='Nome' className='form-control' value={obj.nome} />
+            <input type='text' onChange={eventoTeclado} name='marca' placeholder='Marca' className='form-control' value={obj.marca} />
+
+            {
+                botao ?
+                    <input type='Button' onClick={cadastrar} value='Cadastrar' className='btn btn-primary' />
+                    :
+                    <div>
+                        <input type='Button' onClick={() => alterar()} value='Alterar' className='btn btn-warning' />
+                        <input type='Button' onClick={() => remover()} value='Remover' className='btn btn-danger' />
+                        <input type='Button' onClick={() => cancelar()} value='Cancelar' className='btn btn-secondary' />
+                    </div>
+            }
+        </form>
     )
 }
 
--- front-end/src/Tabela.js
+++ front-end/src/Tabela.js
@@ -1,6 +1,35 @@
-function Tabela() {
+function Tabela({ vetor, selecionar }) {
     return (
-        <h1>Tabela</h1>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Marca</th>
+                    <th>Selecionar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                {
+                    vetor.map((produto, indice) => (
+                        <tr key={indice}>
+                            <td>{produto.id}</td>
+                            <td>{produto.nome}</td>
+                            <td>{produto.marca}</td>
+                            <td><button className="btn btn-success" onClick={() => selecionar(indice)}>Selecionar</button></td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+
     )
 }
 
